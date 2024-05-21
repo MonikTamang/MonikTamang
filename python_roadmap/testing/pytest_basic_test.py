d8514f7d7d6def12504db1d05204565729ac1b7c
MonikTamang <mktmg123@gmail.com>
2024-05-21 19:19:10 +0545

    Organizing the files

---
+++ python_roadmap/testing/pytest_basic_test.py
@@ -0,0 +1,13 @@
+import math
+
+def test_sqrt():
+    num = 25
+    # assert are used to check if the actual result match the expected result
+    assert math.sqrt(num) == 5 
+
+def testsquare():
+    num = 7
+    assert 7*7 == 40
+
+def testquality():
+    assert 10 == 11
