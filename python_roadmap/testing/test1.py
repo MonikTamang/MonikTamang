40a69173ebf6c083a4639da51afeb87de78e33a1
MonikTamang <mktmg123@gmail.com>
2024-05-21 20:12:27 +0545

    updating the test file

---
+++ python_roadmap/testing/test1.py
@@ -0,0 +1,18 @@
+# Function name must include 'test' at the beginning if you want to test the function
+# pytest will run all the test if name is not specified
+
+import math
+
+def test_sqrt():
+    num = 25
+    # assert are used to check if the actual result match the expected result
+    # assertion will generate exception automatically when test donot match the result
+
+    assert math.sqrt(num) == 5  # test passed as square root of 25 is 5
+
+def testsquare():
+    num = 7
+    assert 7*7 == 40  # test failed as 7*7 == 49 
+
+def testquality():
+    assert 10 == 11  # test failed as 10 is not equal to 11
