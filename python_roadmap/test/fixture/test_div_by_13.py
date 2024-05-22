14489536307d122eddac54b014e854bc7775e484
MonikTamang <mktmg123@gmail.com>
2024-05-22 15:38:05 +0545

    creating folders and using fixture function in multiple files

---
+++ python_roadmap/test/fixture/test_div_by_13.py
@@ -0,0 +1,9 @@
+# calling fixture function value (input_value)
+# creating test cases/ functions
+import pytest
+
+def test_divisible_by_13(input_value):
+    assert input_value % 13 == 0, f"{input_value} is not divisible by 3."  # You can also provide the message for AssertionError
+
+# using the substring method to run test
+# pytest -k divisible <substring> -v
